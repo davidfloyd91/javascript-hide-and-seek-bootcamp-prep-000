a0a89b35cc5c52aa6c676d8f2f3d997e1d818f07
David Floyd <davidfloyd91@gmail.com>
2018-10-31 03:07:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+    document
+    .querySelector(selector);
+}
 
+function nestedTarget(nested, target) {
+    document
+    .querySelector(#nested)
+    .querySelector(.target);
+}
+
+function increaseRankBy(n) {
+  var getIt = document
+  .querySelectorAll('.ranked-list');
+  for (i = 0; i < getIt.length; i++) {
+    getIt[i].innerHTML = parseInt(getIt[i].innerHTML) + 1;
+  }
+}
