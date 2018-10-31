29e0ec6fb1af53c95a522db6e8b0a926bdf52576
David Floyd <davidfloyd91@gmail.com>
2018-10-31 04:29:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+    return document
+    .querySelector(selector);
+}
 
+function nestedTarget() {
+    return document
+    .querySelector('#nested')
+    .querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var getIt = document
+  .querySelectorAll('.ranked-list');
+  for (let i = 0; i < getIt.length; i++) {
+    getIt[i].innerHTML = parseInt(getIt[i].innerHTML) + 1;
+  }
+}
+
+function deepestChild() {
+  var lis = document
+  .querySelector('#grand-node')
+  .querySelectorAll('div');
+  var child;
+  for (let i = 0; i < lis.length - 1; i++) {
+    child = lis[i].querySelector('div');
+    }
+  return child;
+}
